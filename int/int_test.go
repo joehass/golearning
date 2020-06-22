80990db14033f2675547819b60ce8645ac4d43a3
huye <huye@1391.com>
2020-06-22 11:53:03 +0800

    int类型

---
+++ int/int_test.go
@@ -0,0 +1,20 @@
+package int
+
+import (
+	"fmt"
+	"testing"
+	"unsafe"
+)
+
+func TestI(t *testing.T) {
+	var i1 int = 1
+	var i2 int8 = 2
+	var i3 int16 = 3
+	var i4 int32 = 4
+	var i5 int64 = 5
+	fmt.Println(unsafe.Sizeof(i1))
+	fmt.Println(unsafe.Sizeof(i2))
+	fmt.Println(unsafe.Sizeof(i3))
+	fmt.Println(unsafe.Sizeof(i4))
+	fmt.Println(unsafe.Sizeof(i5))
+}
