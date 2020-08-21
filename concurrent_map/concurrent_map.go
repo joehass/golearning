16892eaafe8f01c2a7ebf0de80a1cbc60266dd3a
huye <huye@1391.com>
2020-08-21 09:45:13 +0800

    concurrentMap

---
+++ concurrent_map/concurrent_map.go
@@ -0,0 +1,147 @@
+package concurrent_map
+
+import "sync"
+
+var SHARD_COUNT = 32
+
+type ConcurrentMap []*ConcurrentMapShared
+
+type ConcurrentMapShared struct {
+	items map[string]interface{}
+	sync.RWMutex
+}
+
+func New() ConcurrentMap {
+	m := make(ConcurrentMap, SHARD_COUNT)
+	for i := 0; i < SHARD_COUNT; i++ {
+		m[i] = &ConcurrentMapShared{items: map[string]interface{}{}}
+	}
+	return m
+}
+
+func (m ConcurrentMap) GetShard(key string) *ConcurrentMapShared {
+	return m[uint(fnv32(key))%uint(SHARD_COUNT)]
+}
+
+//map的所有元素
+func (m ConcurrentMap) Count() int {
+	count := 0
+	for i := 0; i < SHARD_COUNT; i++ {
+		shard := m[i]
+		shard.RLock()
+		count += len(shard.items)
+		shard.RUnlock()
+	}
+	return count
+}
+
+func (m ConcurrentMap) Set(key string, value interface{}) {
+	shard := m.GetShard(key)
+	shard.Lock()
+	shard.items[key] = value
+	shard.Unlock()
+}
+
+func (m ConcurrentMap) SetIfAbsent(key string, value interface{}) bool {
+	shard := m.GetShard(key)
+	shard.Lock()
+	_, ok := shard.items[key]
+	if !ok {
+		shard.items[key] = value
+	}
+	shard.Unlock()
+	return !ok
+}
+
+func (m ConcurrentMap) Get(key string) (interface{}, bool) {
+	// Get shard
+	shard := m.GetShard(key)
+	shard.RLock()
+	// Get item from shard.
+	val, ok := shard.items[key]
+	shard.RUnlock()
+	return val, ok
+}
+
+func (m ConcurrentMap) Has(key string) bool {
+	// Get shard
+	shard := m.GetShard(key)
+	shard.RLock()
+	// See if element is within shard.
+	_, ok := shard.items[key]
+	shard.RUnlock()
+	return ok
+}
+
+func (m ConcurrentMap) Remove(key string) {
+	// Try to get shard.
+	shard := m.GetShard(key)
+	shard.Lock()
+	delete(shard.items, key)
+	shard.Unlock()
+}
+
+type RemoveCb func(key string, v interface{}, exists bool) bool
+
+//满足cb条件后删除
+func (m ConcurrentMap) RemoveCb(key string, cb RemoveCb) bool {
+	shard := m.GetShard(key)
+	shard.Lock()
+	v, ok := shard.items[key]
+	remove := cb(key, v, ok)
+	if remove && ok {
+		delete(shard.items, key)
+	}
+	shard.Unlock()
+	return remove
+}
+
+func (m ConcurrentMap) Pop(key string) (v interface{}, exists bool) {
+	// Try to get shard.
+	shard := m.GetShard(key)
+	shard.Lock()
+	v, exists = shard.items[key]
+	delete(shard.items, key)
+	shard.Unlock()
+	return v, exists
+}
+
+func (m ConcurrentMap) Keys() []string {
+	count := m.Count()
+	ch := make(chan string, count)
+	go func() {
+		wg := sync.WaitGroup{}
+		wg.Add(SHARD_COUNT)
+		for _, shard := range m {
+			go func(shard *ConcurrentMapShared) {
+				shard.RLock()
+				for key := range shard.items {
+					ch <- key
+				}
+			}(shard)
+		}
+		wg.Wait()
+		close(ch)
+	}()
+
+	keys := make([]string, 0, count)
+	for k := range ch {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+func (m ConcurrentMap) IsEmpty() bool {
+	return m.Count() == 0
+}
+
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	const prime32 = uint32(16777619)
+	for i := 0; i < len(key); i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
---
+++ concurrent_map/map_test.go
@@ -0,0 +1,280 @@
+package concurrent_map
+
+import (
+	"strconv"
+	"testing"
+)
+
+type Animal struct {
+	name string
+}
+
+func TestMapCreation(t *testing.T) {
+	m := New()
+	if m == nil {
+		t.Error("map is null")
+	}
+
+	if m.Count() != 0 {
+		t.Error("new map should be empty.")
+	}
+}
+
+func TestInsert(t *testing.T) {
+	m := New()
+
+	elephant := Animal{"elephant"}
+	monkey := Animal{"monkey"}
+
+	m.Set("elephant", elephant)
+	m.Set("monkey", monkey)
+
+	if m.Count() != 2 {
+		t.Error("map should contain exactly two elements.")
+	}
+}
+
+func TestInsertAbsent(t *testing.T) {
+	m := New()
+	elephant := Animal{"elephant"}
+	monkey := Animal{"monkey"}
+
+	m.SetIfAbsent("elephant", elephant)
+	if ok := m.SetIfAbsent("elephant", monkey); ok {
+		t.Error("map set a new value even the entry is already present")
+	}
+}
+
+func TestGet(t *testing.T) {
+	m := New()
+
+	// Get a missing element.
+	val, ok := m.Get("Money")
+
+	if ok == true {
+		t.Error("ok should be false when item is missing from map.")
+	}
+
+	if val != nil {
+		t.Error("Missing values should return as null.")
+	}
+
+	elephant := Animal{"elephant"}
+	m.Set("elephant", elephant)
+
+	// Retrieve inserted element.
+	tmp, ok := m.Get("elephant")
+	if ok == false {
+		t.Error("ok should be true for item stored within the map.")
+	}
+
+	elephant, ok = tmp.(Animal) // Type assertion.
+	if !ok {
+		t.Error("expecting an element, not null.")
+	}
+
+	if elephant.name != "elephant" {
+		t.Error("item was modified.")
+	}
+}
+
+func TestHas(t *testing.T) {
+	m := New()
+
+	// Get a missing element.
+	if m.Has("Money") == true {
+		t.Error("element shouldn't exists")
+	}
+
+	elephant := Animal{"elephant"}
+	m.Set("elephant", elephant)
+
+	if m.Has("elephant") == false {
+		t.Error("element exists, expecting Has to return True.")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	m := New()
+
+	monkey := Animal{"monkey"}
+	m.Set("monkey", monkey)
+
+	m.Remove("monkey")
+
+	if m.Count() != 0 {
+		t.Error("Expecting count to be zero once item was removed.")
+	}
+
+	temp, ok := m.Get("monkey")
+
+	if ok != false {
+		t.Error("Expecting ok to be false for missing items.")
+	}
+
+	if temp != nil {
+		t.Error("Expecting item to be nil after its removal.")
+	}
+
+	// Remove a none existing element.
+	m.Remove("noone")
+}
+
+func TestRemoveCb(t *testing.T) {
+	m := New()
+
+	monkey := Animal{"monkey"}
+	m.Set("monkey", monkey)
+	elephant := Animal{"elephant"}
+	m.Set("elephant", elephant)
+
+	var (
+		mapKey   string
+		mapVal   interface{}
+		wasFound bool
+	)
+
+	cb := func(key string, val interface{}, exists bool) bool {
+		mapKey = key
+		mapVal = val
+		wasFound = exists
+
+		if animal, ok := val.(Animal); ok {
+			return animal.name == "monkey"
+		}
+		return false
+	}
+	result := m.RemoveCb("monkey", cb)
+	if !result {
+		t.Errorf("Result was not true")
+	}
+
+	if mapKey != "monkey" {
+		t.Error("Wrong key was provided to the callback")
+	}
+
+	if mapVal != monkey {
+		t.Errorf("Wrong value was provided to the value")
+	}
+
+	if !wasFound {
+		t.Errorf("Key was not found")
+	}
+
+	if m.Has("monkey") {
+		t.Errorf("Key was not removed")
+	}
+
+	// Elephant should not be removed
+	result = m.RemoveCb("elephant", cb)
+	if result {
+		t.Errorf("Result was true")
+	}
+
+	if mapKey != "elephant" {
+		t.Error("Wrong key was provided to the callback")
+	}
+
+	if mapVal != elephant {
+		t.Errorf("Wrong value was provided to the value")
+	}
+
+	if !wasFound {
+		t.Errorf("Key was not found")
+	}
+
+	if !m.Has("elephant") {
+		t.Errorf("Key was removed")
+	}
+
+	// Unset key should remain unset
+	result = m.RemoveCb("horse", cb)
+	if result {
+		t.Errorf("Result was true")
+	}
+
+	if mapKey != "horse" {
+		t.Error("Wrong key was provided to the callback")
+	}
+
+	if mapVal != nil {
+		t.Errorf("Wrong value was provided to the value")
+	}
+
+	if wasFound {
+		t.Errorf("Key was found")
+	}
+
+	if m.Has("horse") {
+		t.Errorf("Key was created")
+	}
+
+}
+
+func TestPop(t *testing.T) {
+	m := New()
+
+	monkey := Animal{"monkey"}
+	m.Set("monkey", monkey)
+
+	v, exists := m.Pop("monkey")
+
+	if !exists {
+		t.Error("Pop didn't find a monkey.")
+	}
+
+	m1, ok := v.(Animal)
+
+	if !ok || m1 != monkey {
+		t.Error("Pop found something else, but monkey.")
+	}
+
+	v2, exists2 := m.Pop("monkey")
+	m1, ok = v2.(Animal)
+
+	if exists2 || ok || m1 == monkey {
+		t.Error("Pop keeps finding monkey")
+	}
+
+	if m.Count() != 0 {
+		t.Error("Expecting count to be zero once item was Pop'ed.")
+	}
+
+	temp, ok := m.Get("monkey")
+
+	if ok != false {
+		t.Error("Expecting ok to be false for missing items.")
+	}
+
+	if temp != nil {
+		t.Error("Expecting item to be nil after its removal.")
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	m := New()
+
+	if m.IsEmpty() == false {
+		t.Error("new map should be empty")
+	}
+
+	m.Set("elephant", Animal{"elephant"})
+
+	if m.IsEmpty() != false {
+		t.Error("map shouldn't be empty.")
+	}
+}
+
+func TestKeys(t *testing.T) {
+	m := New()
+
+	// Insert 100 elements.
+	for i := 0; i < 100; i++ {
+		m.Set(strconv.Itoa(i), Animal{strconv.Itoa(i)})
+	}
+
+	keys := m.Keys()
+	if len(keys) != 100 {
+		t.Error("We should have counted 100 elements.")
+	}
+}
