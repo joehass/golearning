226c051826b7892296ccd911e3b1dae903a131d7
huye <huye@1391.com>
2020-08-31 09:56:20 +0800

    冒泡排序

--- sort/bubble/bubbleSort_test.go
+++ sort/bubble/bubbleSort_test.go
@@ -5,14 +5,14 @@ import (
 	"testing"
 )
 
-func bubbleSort(nums *[]int) {
+func bubbleSort(nums []int) {
 	//控制比较次数
-	for i := 0; i < len(*nums); i++ {
+	for i := 0; i < len(nums); i++ {
 		////j<len(*nums)-i:是为了不去比较已经到最后的最大数据
 		//j =1:是为了每次都要从开始比较
-		for j := 1; j < len(*nums)-i; j++ {
-			if (*nums)[j] < (*nums)[j-1] {
-				(*nums)[j], (*nums)[j-1] = (*nums)[j-1], (*nums)[j]
+		for j := 1; j < len(nums)-i; j++ {
+			if nums[j] < nums[j-1] {
+				nums[j], nums[j-1] = nums[j-1], nums[j]
 			}
 		}
 	}
@@ -20,6 +20,6 @@ func bubbleSort(nums *[]int) {
 
 func TestSort(t *testing.T) {
 	nums := []int{1, 9, 5, 3, 1, 6, 2}
-	bubbleSort(&nums)
+	bubbleSort(nums)
 	fmt.Println(nums)
 }
